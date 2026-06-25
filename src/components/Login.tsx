61fe9a8b58fea040d0fbf6115aca8b789df12480
vuhungthcssuoilu-cloud <vuhungthcssuoilu@gmail.com>
2026-06-25 15:01:41 +0700

    Update Login.tsx

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -57,10 +57,10 @@ export default function Login() {
   const [uiConfigs, setUiConfigs] = useState({
     school_name: 'TRƯỜNG PTDTBT TH VÀ THCS SUỐI LƯ',
     header_title: 'HỆ THỐNG QUẢN LÝ NỘI TRÚ - SỔ CHẤM CƠM',
-    school_year: 'NĂM HỌC 2024 - 2025',
-    footer_line1: 'DỮ LIỆU CHÍNH THỨC TỪ TRƯỜNG PTDTBT TH VÀ THCS SUỐI LƯ',
+    school_year: 'NĂM HỌC 2026 - 2027',
+    footer_line1: '',
     footer_line2: 'Mọi thắc mắc về phần mềm xin liên hệ quản trị viên',
-    footer_line3: 'Application developed by: Vũ Hùng - Email: vuhung@db.edu.vn - SĐT: 0984.246.993'
+    footer_line3: 'Application developed by: Vũ Hùng - Email: vuhung@db.edu.vn'
   });
 
   useEffect(() => {
